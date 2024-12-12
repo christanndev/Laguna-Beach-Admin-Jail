3576422d4210ac15d1f154293cb1121fdc9ef06b
christanndev <147557637+christanndev@users.noreply.github.com>
2024-12-13 00:04:42 +0100

    Add files via upload

---
+++ adminJail.js
@@ -0,0 +1,55 @@
+const AdminJail = new Vue({
+    el: ".AdminJail",
+    data: {
+        active: true,
+        zoom: 1,
+        time: '12:00'
+    },
+
+    mounted() {
+        this.handleResize();
+        window.addEventListener("resize", this.handleResize);
+        window.addEventListener("message", this.onMessage);
+    },
+
+    methods: {
+        async post(url, data = {}) {
+            try {
+                const response = await fetch(`https://${GetParentResourceName()}/${url}`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(data)
+                });
+            
+                if (response.ok) {
+                    return await response.json();
+                } else {
+                    throw new Error(`${response.status}`);
+                }
+            } catch (error) {
+                return null;
+            }
+        },
+
+        handleResize() {
+            var zoomCountOne = window['innerWidth'] / 1920;
+            var zoomCountTwo = window['innerHeight'] / 1080;
+
+            if (zoomCountOne < zoomCountTwo) {
+                this['zoom'] = zoomCountOne;
+            } else {
+                this['zoom'] = zoomCountTwo;
+            }
+        },
+
+        onMessage() {
+            var data = event.data
+
+            switch (data.act) {
+                case "Open":
+                    
+                break;
+            }
+        }
+    }
+});
